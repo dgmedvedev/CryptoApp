5ce9885b9f6a4d58e27d0d22ef57cea0b21dcc0d
dgmedvedev <dg-medvedev@mail.ru>
2023-08-09 20:00:04 +0300

    database complete

--- app/src/main/java/com/demo/cryptoapp/pojo/CoinPriceInfoRawData.kt
+++ app/src/main/java/com/demo/cryptoapp/pojo/CoinPriceInfoRawData.kt
@@ -2,7 +2,6 @@ package com.demo.cryptoapp.pojo
 
 import com.google.gson.JsonObject
 import com.google.gson.annotations.Expose
-
 import com.google.gson.annotations.SerializedName
 
 data class CoinPriceInfoRawData(
