973ffb507476bf01c2aabfaface832c5a35e2b66
dgmedvedev <dg-medvedev@mail.ru>
2023-08-09 18:10:26 +0300

    api complete

--- app/src/main/java/com/demo/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/demo/cryptoapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.demo.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.demo.cryptoapp.api.ApiFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val disposable = ApiFactory.apiService.getFullPriceList(fSyms = "BTC,XRP")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST_OF_LOADING_DATA", it.toString())
+            }, {
+                Log.d("TEST_OF_LOADING_DATA", it.message.toString())
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
