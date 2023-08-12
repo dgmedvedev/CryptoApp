0b2a0c82d769397935a7a2370ad6507c5fe9c746
dgmedvedev <dg-medvedev@mail.ru>
2023-08-12 10:14:56 +0300

    layouts complete add getFormattedTime(),getFullImageUrl()

--- app/src/main/java/com/demo/cryptoapp/pojo/CoinPriceInfo.kt
+++ app/src/main/java/com/demo/cryptoapp/pojo/CoinPriceInfo.kt
@@ -2,6 +2,8 @@ package com.demo.cryptoapp.pojo
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.demo.cryptoapp.api.ApiFactory
+import com.demo.cryptoapp.utils.convertTimestampToTime
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
@@ -28,7 +30,7 @@ data class CoinPriceInfo(
     val price: Double?,
     @SerializedName("LASTUPDATE")
     @Expose
-    val lastUpdate: Int?,
+    val lastUpdate: Long?,
     @SerializedName("MEDIAN")
     @Expose
     val median: Double?,
@@ -152,4 +154,12 @@ data class CoinPriceInfo(
     @SerializedName("IMAGEURL")
     @Expose
     val imageUrl: String?
-)
+) {
+    fun getFormattedTime() {
+        convertTimestampToTime(lastUpdate)
+    }
+
+    fun getFullImageUrl(): String {
+        return ApiFactory.BASE_IMAGE_URL + imageUrl
+    }
+}
