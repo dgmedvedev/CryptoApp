15e1af8041106d37857b8bd109baa055ef8515b9
dgmedvedev <dg-medvedev@mail.ru>
2023-08-26 23:05:18 +0300

    delete CoinFavouriteActivity. the adapter is now used in CoinPriceListActivity.kt

--- app/src/main/java/com/demo/cryptoapp/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/demo/cryptoapp/presentation/CoinPriceListActivity.kt
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.demo.cryptoapp.R
 import com.demo.cryptoapp.databinding.ActivityCoinPriceListBinding
+import com.demo.cryptoapp.domain.CoinFavouriteInfo
 import com.demo.cryptoapp.domain.CoinInfo
+import com.demo.cryptoapp.presentation.adapters.CoinFavouriteInfoAdapter
 import com.demo.cryptoapp.presentation.adapters.CoinInfoAdapter
 
 class CoinPriceListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCoinPriceListBinding
+    private lateinit var coinInfoAdapter: CoinInfoAdapter
+    private lateinit var coinFavouriteInfoAdapter: CoinFavouriteInfoAdapter
 
     private val viewModel: CoinViewModel by lazy {
         ViewModelProvider(this)[CoinViewModel::class.java]
@@ -21,7 +25,44 @@ class CoinPriceListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCoinPriceListBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val adapter = CoinInfoAdapter(this)
+        coinInfoAdapter = CoinInfoAdapter(this)
+        coinFavouriteInfoAdapter = CoinFavouriteInfoAdapter(this)
+
+        setClickListeners(coinInfoAdapter, coinFavouriteInfoAdapter)
+
+        binding.rvCoinPriceList.adapter = coinInfoAdapter
+        binding.rvCoinPriceList.itemAnimator = null
+
+        viewModel.coinInfoList.observe(this) {
+            coinInfoAdapter.submitList(it)
+        }
+
+        viewModel.coinFavouriteInfoList.observe(this) {
+            coinFavouriteInfoAdapter.submitList(it)
+        }
+
+        binding.tvFavourite.setOnClickListener {
+
+        }
+
+        binding.switchList.isChecked = false
+        binding.switchList.setOnCheckedChangeListener { compoundButton, isChecked ->
+            changeAdapter(isChecked)
+        }
+    }
+
+    private fun changeAdapter(isChecked: Boolean) {
+        if (isChecked) {
+            binding.rvCoinPriceList.adapter = coinFavouriteInfoAdapter
+        } else {
+            binding.rvCoinPriceList.adapter = coinInfoAdapter
+        }
+    }
+
+    private fun setClickListeners(
+        adapter: CoinInfoAdapter,
+        favouriteAdapter: CoinFavouriteInfoAdapter
+    ) {
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinInfo: CoinInfo) {
                 if (binding.fragmentContainer == null) {
@@ -40,16 +81,25 @@ class CoinPriceListActivity : AppCompatActivity() {
                 ).show()
             }
         }
-        binding.rvCoinPriceList.adapter = adapter
-        binding.rvCoinPriceList.itemAnimator = null
+        favouriteAdapter.onCoinClickListener =
+            object : CoinFavouriteInfoAdapter.OnCoinClickListener {
+                override fun onCoinClick(coinFavouriteInfo: CoinFavouriteInfo) {
+                    if (binding.fragmentContainer == null) {
+                        launchDetailActivity(coinFavouriteInfo.fromSymbol)
+                    } else {
+                        launchDetailFragment(coinFavouriteInfo.fromSymbol)
+                    }
+                }
 
-        viewModel.coinInfoList.observe(this) {
-            adapter.submitList(it)
-        }
-
-        binding.tvFavourite.setOnClickListener {
-            launchFavouriteActivity()
-        }
+                override fun onCoinLongClick(coinFavouriteInfo: CoinFavouriteInfo) {
+                    viewModel.stopAllWorkers()
+                    Toast.makeText(
+                        applicationContext,
+                        getString(R.string.background_update_disabled),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 
     private fun launchDetailActivity(fromSymbol: String) {
@@ -57,11 +107,6 @@ class CoinPriceListActivity : AppCompatActivity() {
             this@CoinPriceListActivity,
             fromSymbol
         )
-        startActivity(intent)
-    }
-
-    private fun launchFavouriteActivity() {
-        val intent = CoinFavouriteActivity.newIntent(this)
         startActivity(intent)
     }
 
